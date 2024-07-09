f7adf7a45ed499ca22786f242cc4bd12057631db
Yashwant <yashwant08012005@gmail.com>
2024-07-09 17:47:10 +0530

    fixed typo

--- src/utils/calculateScore.js
+++ src/utils/calculateScore.js
@@ -1,4 +1,4 @@
-import { selectedAnswers } from "../components/question";
+import { selectedAnswers } from "../components/mcqTemplate";
 export default function calculateScore(questions){
    let score = 0;
     for(let i=0;i<questions.length;i++){
